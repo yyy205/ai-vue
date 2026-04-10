0fa85a787198ee5312027bb791ffee064108fb66
yyy205 <tianhao100804@gmail.com>
2026-04-10 18:13:16 +0800

    feat: "TableSearch组件的封装"

---
+++ src/components/TableSearch.vue
@@ -0,0 +1,25 @@
+<template>
+  <el-form>
+    <template v-for="item in formItems" :key="item.prop">
+      <el-form-item :label="item.label" :prop="item.prop">
+        <component :is="isComp(item.comp)" :placeholder="item.placeholder"></component>
+      </el-form-item>
+    </template>
+  </el-form>
+</template>
+  
+<script setup>
+  const props = defineProps({
+      formItems: {
+        type: Array,
+        default: () => []
+      }
+  })
+
+  const isComp = (comp) =>{
+    return{
+       input: 'elInput',
+       select: 'elSelect'
+    }[comp]
+  }
+</script>
