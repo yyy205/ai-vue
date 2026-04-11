e2eaf54b8c54a3263f1ed36efbc77c8a7d5bb24e
yyy205 <tianhao100804@gmail.com>
2026-04-11 19:16:46 +0800

    feat: "TableSearch组件封装完毕"

--- src/components/TableSearch.vue
+++ src/components/TableSearch.vue
@@ -1,20 +1,63 @@
 <template>
-  <el-form>
-    <template v-for="item in formItems" :key="item.prop">
-      <el-form-item :label="item.label" :prop="item.prop">
-        <component :is="isComp(item.comp)" :placeholder="item.placeholder"></component>
-      </el-form-item>
-    </template>
+  <el-form ref="ruleFormRef" :model="formData">
+    <el-row :gutter="24">
+      <template v-for="item in formItemAttrs" :key="item.prop">
+        <el-col v-bind="item.col">
+          <el-form-item :label="item.label" :prop="item.prop">
+            <component v-model="formData[item.prop]":is="isComp(item.comp)" :placeholder="item.placeholder">
+              <template v-if="item.comp === 'select'">
+                <el-option label="全部" value=""></el-option>
+                <el-option
+                  v-for="opt in item.options"
+                  :key="opt.value"
+                  :label="opt.label"
+                  :value="opt.value"
+                ></el-option>
+              </template>
+            </component>
+          </el-form-item>
+        </el-col>
+      </template>
+    </el-row>
+    <el-row>
+      <el-button type="primary" @click="handleSearch">查询</el-button>
+      <el-button @click="handleReset(ruleFormRef)">重置</el-button>
+    </el-row>
   </el-form>
 </template>
   
 <script setup>
+import { ref, reactive, computed} from 'vue';
+
+const ruleFormRef = ref()
+const formData = reactive({})
+
+
   const props = defineProps({
-      formItems: {
+      formItem: {
         type: Array,
         default: () => []
       }
   })
+
+  const emit =defineEmits(['search'])
+
+  const formItemAttrs = computed(() => {
+    const { formItem } = props
+    formItem.forEach(item =>{
+      item.col = { xs:24,sm:12,md:8,lg:6,xl:6}
+    })
+    return formItem
+  })
+
+  const handleSearch = () => {
+    emit('search',formData)
+  }
+
+  const handleReset = (formEl) => {
+    if(!formEl) return
+      formEl.resetFields()
+  }
 
   const isComp = (comp) =>{
     return{
