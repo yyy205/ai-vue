fe988fee7d70ee05712450925ba693dcc5af320c
yyy205 <tianhao100804@gmail.com>
2026-05-09 19:35:25 +0800

    feat:"1.v-if详情功能和删除键的完善 2.数据分析实现（顶部+趋势：Echarts)"

--- src/api/admin.js
+++ src/api/admin.js
@@ -68,3 +68,11 @@ export function getSessionDetail(sessionId) {
 export function getEmotionalPage(params) {
   return service.get('/emotion-diary/admin/page',{params})
 }
+
+export function deleteEmotional(id) {
+  return service.delete(`/emotion-diary/admin/${id}`)
+}
+
+export function getAnalyticsOverview() {
+  return service.get(`/data-analytics/overview`)
+}
